88c961b9f1051a048a5d04813c0ae531e3edbf2e
igorbsilva1901 <108560697+igorbsilva1901@users.noreply.github.com>
2023-01-18 12:54:32 -0300

    Cria borda e janela para criação de Menu
    Bordas para Menu

--- gorillas.c
+++ gorillas.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <curses.h>
 
-int main(void) {
-	initscr();
+int menu(void){
+
+        int xmax, ymax;
+        initscr();
+        noecho();
+
+        getmaxyx(stdscr,ymax,xmax);
+
+        mvprintw((ymax/2),(xmax/2),"Hello World");
+
+        WINDOW * janela = newwin((ymax-5),(xmax-5),2,2);
+        box(janela,0,0);
+        refresh();
+        wrefresh(janela);
+
+        getch();
+        endwin();
+
+        return 0;
+
+}
+
+int main(void){
+
+        menu();
 
 
 
-
-
-
-}	
+}
