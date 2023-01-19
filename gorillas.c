c6edbe3b560bc6b5102d0c1642f5b7c02b990737
A-nakarlad <114787650+A-nakarlad@users.noreply.github.com>
2023-01-19 16:07:29 -0300

    Modificações no menu
    - endwin tava dentro do laço e por isso ficava o efeito de fechar janela e abrir novamente
    - tinha que apertar duas vezes por causa do getch dentro do laço
    - modificação na posição das opções pra adaptar a cada tam de console

--- gorillas.c
+++ gorillas.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <curses.h>
 
-<<<<<<< HEAD
-int xmax, ymax;
-int escolha, ValorLevel;
-
-int mudarCor(char x[3][10], char y[]) //mudar cor ao usar setas down e up
-{
-=======
 int menu(void){
     int xmax, ymax;
     char menu[3][10] = {"Playing", "Ranking", "Exit"};
->>>>>>> c6b4fd7ed2786dbd73c5c59150a270339d1604be
     int index = 0;
+    int escolha;
+
+
+    initscr();
+    noecho();
     getmaxyx(stdscr,ymax,xmax);
+
     WINDOW * janela = newwin((ymax-5),(xmax-5),2,2);
     box(janela,0,0);
     refresh();
     wrefresh(janela);
 
     keypad(janela, true);
-    mvwprintw(janela, 10, (xmax/2)-5, "%s", y);
-
+    mvwprintw(janela, 10, (xmax/2)-4, "GORILLAS");
     while(true) {
         for(int i=0; i < 3; i++) {
             if (i == index) {
                 wattron(janela, A_REVERSE);
-                mvwprintw(janela, i+15, (xmax/2)-4, "%s", x[i]);
+                mvwprintw(janela, i+15, (xmax/2)-4, "%s", menu[i]);
                 wattroff(janela, A_REVERSE);
             } else {
-                mvwprintw(janela, i+15, (xmax/2)-4, "%s", x[i]);
+                mvwprintw(janela, i+15, (xmax/2)-4, "%s", menu[i]);
             }
         }
         escolha = wgetch(janela);
@@ -45,65 +42,24 @@ int menu(void){
                 if(index == 3)
                     index = 0;
                 break;
+            case 10: //ENTER EM ASCII
+                wclear(janela);
+                clear();
+                mvprintw( 2, 7, "voce escolheu: %s", menu[index]);
+                break;
             default:
                 break;
         }
-        if(escolha == 10){
-            break;
-        }
+        if(escolha == 10)
+                break;
     }
-   return index;
-}
-int menu(void){
-    char menu[3][10] = {"Playing", "Ranking", "Exit"};
-    char titulo[] = {"GORILLAS"};
-    int picklevel (void);
-
-    initscr();
-    noecho();
-
-    WINDOW * janela = newwin((ymax-5),(xmax-5),2,2);
-    box(janela,0,0);
-    refresh();
-    wrefresh(janela);
-    int index = mudarCor(menu, titulo);
-
-    if (menu[index] == menu[0])
-        picklevel();
-    //else if (menu[index] == menu[1])
-        //ranking();
-
     getch();
     endwin();
 
+
 }
-int picklevel (void)
-{
-    char dificuldade[3][10] = {"Facil", "Normal", "Dificil"};
-    char titulo[] = {"ESCOLHA UMA DIFICULDADE"};
-
-    WINDOW * janela = newwin((ymax-5),(xmax-5),2,2);
-    box(janela,0,0);
-    refresh();
-    wrefresh(janela);
-
-
-    int index = mudarCor(dificuldade, titulo);
-
-    if (dificuldade[index] == dificuldade[0])
-        ValorLevel = 30;
-    else if (dificuldade[index] == dificuldade[1])
-        ValorLevel = 25;
-    else if (dificuldade[index] == dificuldade[2])
-        ValorLevel = 20;
-    printw("Voce escolheu: %s", dificuldade[index]);
-}
-
-
-
 
 int main(void){
 
-    menu();
+        menu();
 }
-
