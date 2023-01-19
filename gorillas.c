7ee40a662e40895b098b842ce278ef8ea7efe0eb
A-nakarlad <114787650+A-nakarlad@users.noreply.github.com>
2023-01-19 12:55:28 -0300

    Modificações no menu
    - endwin tava dentro do laço e por isso ficava o efeito de fechar janela e abrir novamente
    - tinha que apertar duas vezes por causa do getch dentro do laço
    - modificação na posição das opções pra adaptar a cada tam de console

--- gorillas.c
+++ gorillas.c
@@ -1,61 +1,61 @@
 #include <stdio.h>
 #include <curses.h>
 
-int menu(void){
+void menu(void){
+    int xmax, ymax;
+    char menu[3][10] = {"Playing", "Ranking", "Exit"};
+    int index = 0;
+    int escolha;
 
-        int xmax, ymax;
-        char menu[3][10] = {"Playing", "Ranking", "Exit"};
-        int index = 0;
-        int escolha;
-        
-        initscr();
-        noecho();
-        getmaxyx(stdscr,ymax,xmax);
 
-        WINDOW * janela = newwin((ymax-5),(xmax-5),2,2);
-        box(janela,0,0);
-        refresh();
-        wrefresh(janela);
+    initscr();
+    noecho();
+    getmaxyx(stdscr,ymax,xmax);
 
-        keypad(janela, true);
+    WINDOW * janela = newwin((ymax-5),(xmax-5),2,2);
+    box(janela,0,0);
+    refresh();
+    wrefresh(janela);
 
-        while(true) {
-                mvwprintw(janela, 10, 70, "GORILLAS");
-                for(int i=0; i < 3; i++) {
-                        if (i == index) {
-                                wattron(janela, A_REVERSE);
-                                mvwprintw(janela, i+15, 70, "%s", menu[i]);
-                                wattroff(janela, A_REVERSE);
-                        } else {
-                                mvwprintw(janela, i+15, 70, "%s", menu[i]);
-                        }
-                }
-                escolha = wgetch(janela);
-				
+    keypad(janela, true);
+    mvwprintw(janela, 10, (xmax/2)-4, "GORILLAS");
+    while(true) {
+        for(int i=0; i < 3; i++) {
+            if (i == index) {
+                wattron(janela, A_REVERSE);
+                mvwprintw(janela, i+15, (xmax/2)-4, "%s", menu[i]);
+                wattroff(janela, A_REVERSE);
+            } else {
+                mvwprintw(janela, i+15, (xmax/2)-4, "%s", menu[i]);
+            }
+        }
+        escolha = wgetch(janela);
+
         switch(escolha) {
-                case KEY_UP:
+            case KEY_UP:
                 index--;
                 if(index == -1)
-                        index = 0;
+                    index = 2;
                 break;
-                case KEY_DOWN:
+            case KEY_DOWN:
                 index++;
-                if(index == 3) 
-                        index = 2;
+                if(index == 3)
+                    index = 0;
                 break;
-                case 10: //ENTER EM ASCII
+            case 10: //ENTER EM ASCII
                 wclear(janela);
                 clear();
-                mvprintw(2, 7, "voce escolheu: %s",menu[escolha]);
+                mvprintw( 2, 7, "voce escolheu: %s", menu[index]);
                 break;
-                default:
+            default:
                 break;
-                if(escolha == 10)
-                        break;         
         }
-		getch();
-                endwin();
-        }
+        if(escolha == 10)
+                break;
+    }
+    getch();
+    endwin();
+
 
 }
 
@@ -63,3 +63,4 @@ int main(void){
 
         menu();
 }
+
